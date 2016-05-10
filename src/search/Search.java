195630ad37ff0d2985daca7183016a94178eea95
Evan Hallmark <ehallmark@gttgrp.com>
2016-05-10 07:37:22 -0700

    default fast option

--- src/search/Search.java
+++ src/search/Search.java
@@ -36,14 +36,12 @@ public class Search {
 					+ "<label style='margin-right:15px;'>Patent Number:</label><input id='patent' name='patent' /><br/>"
 					+ "<label style='margin-right:15px;'>Limit:</label><input name='limit' id='limit' value='100' /><br/>"
 					+ "<label style='margin-right:15px;'>Technology Estimate?</label><input name='technology' id='technology' type='checkbox' value='yes' /><br/>"
-					+ "<label style='margin-right:15px;'>Fast?</label><input name='fast' id='fast' type='checkbox' value='yes' /><br/>"
 					+ "<button>Find Similar Patents</button>"
 				+ "</form><br/>"
 				+ "<h3>By Raw Text</h3>"
 				+ "<form action='/find_by_text' method='post'>"
 					+ "<button>Find Similar Patents</button><br/><br/>"
 					+ "<label style='margin-right:15px;'>Limit:</label><input name='limit' id='limit' value='100' /><br/>"
-					+ "<label style='margin-right:15px;'>Fast?</label><input name='fast' id='fast' type='checkbox' value='yes' /><br/>"
 					+ "<textarea rows='10' cols='50' id='text' name='text' ></textarea>"
 				+ "</form>";
 			template.add(html);
@@ -58,10 +56,6 @@ public class Search {
 			try{ limit = Integer.parseInt(req.queryParams("limit")); } catch(Exception e){ limit = 100; }
 			if(patent==null) return "Please provide a patent number!";
 			else {patent=patent.toUpperCase().trim().replaceAll("[^0-9A-Z]", "");}
-
-			boolean fast;
-			if(req.queryParams("fast")==null) fast = false;
-			else fast = true;
 			
 			boolean withTechnologies;
 			if(req.queryParams("technology")==null) withTechnologies = false;
@@ -69,7 +63,7 @@ public class Search {
 		
 			try {
 				synchronized(Database.class) {
-					results = Database.similarPatents(patent,limit,fast);
+					results = Database.similarPatents(patent,limit,true);
 				}
 				if(results == null) {
 					return "Patent not found!";
@@ -119,10 +113,6 @@ public class Search {
 			try{ limit = Integer.parseInt(req.queryParams("limit")); } catch(Exception e){ limit = 100; }
 			if(text==null) return "Please provide some text!";
 			
-			boolean fast;
-			if(req.queryParams("fast")==null) fast = false;
-			else fast = true;
-			
 			// Create min hash for this text
 			Vector<Integer> MinHashVector;
 			try {
@@ -135,7 +125,7 @@ public class Search {
 			
 			try {
 				synchronized(Database.class) {
-					results = Database.similarPatents(MinHashVector,limit,fast);
+					results = Database.similarPatents(MinHashVector,limit,true);
 				}
 			} catch (SQLException sql) {
 				sql.printStackTrace();
