49a1e4783d2ba24372a4581dcf9fbadf87faa861
Evan Hallmark <ehallmark@gttgrp.com>
2016-05-27 11:11:54 -0700

    changed table indices to match Main

--- descriptionSchema.sql
+++ descriptionSchema.sql
@@ -21,7 +21,7 @@ ALTER TABLE patent_description_min_hash ADD COLUMN pub_doc_number varchar(25) PR
 DO language 'plpgsql'
 $$
 DECLARE patent_description_index text :=  string_agg('CREATE INDEX desc' || i::text || '_index ON patent_description_min_hash (m' || i::text || ',m' || (i+1)::text || ',m' || (i+2)::text || ',m'||(i+3)::text||');', ' ') 
-    FROM generate_series(1,200,4) As i;
+    FROM generate_series(1,80,4) As i;
 BEGIN
     EXECUTE patent_description_index; 
 END;
