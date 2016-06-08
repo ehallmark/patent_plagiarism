951cb36420108625bf64335c16494eb7e2cf2415
Evan Hallmark <ehallmark@gttgrp.com>
2016-06-08 10:18:21 -0700

    last change before mac mini

--- shingles.sql
+++ shingles.sql
@@ -4,3 +4,4 @@ $$ BEGIN
     RETURN ARRAY_TO_STRING(ARRAY_AGG(regexp_replace(coalesce(lower(temp.result),''), '[^a-z]', '', 'g')), '') FROM (SELECT * from UNNEST(STRING_TO_ARRAY(t,' ')) as result where char_length(regexp_replace(result, '[^a-z]', '', 'g')) > 2) as temp; 
    END
 $$ LANGUAGE plpgsql;
+
