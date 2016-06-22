828457e5c8a41423a76f9ddda1eff2bb05da5d40
Evan Hallmark <ehallmark@gttgrp.com>
2016-06-22 09:02:53 -0700

    threaded

--- src/seed/Patent.java
+++ src/seed/Patent.java
@@ -24,27 +24,8 @@ public class Patent {
 					if (rs.next()) {
 						String[] claims = (String[]) rs.getArray(1).getArray();
 						Integer[] numbers = (Integer[]) rs.getArray(2).getArray();
-						RecursiveTask<Integer[]> innerAction = new RecursiveTask<Integer[]>() {
-							public Integer[] compute() {
-								Integer[] claimCache2 = new Integer[Main.NUM_HASH_FUNCTIONS_CLAIM];
-								for (int i = 0; i < claims.length/2; i++) {
-									try {
-										List<Integer> curr = NLP.createMinHash(claims[i], SimilarityType.CLAIM, Main.LEN_SHINGLES);
-										if (curr == null) continue;
-										Database.updateClaimMinHash(curr, obj.name, numbers[i]);
-										for (int j = 0; j < curr.size(); j++) {
-											if (claimCache2[j] > curr.get(j)) claimCache2[j] = curr.get(j);
-										}
-									} catch (Exception e) {
-									}
 
-								}
-								return claimCache2;
-							}
-						};
-						innerAction.fork();
-
-						for (int i = claims.length/2; i < claims.length; i++) {
+						for (int i = claims.length; i < claims.length; i++) {
 							try {
 								List<Integer> curr = NLP.createMinHash(claims[i], SimilarityType.CLAIM, Main.LEN_SHINGLES);
 								if (curr == null) continue;
@@ -57,12 +38,6 @@ public class Patent {
 
 						}
 						try {
-							Integer[] received = innerAction.get();
-							if(received!=null) {
-								for (int i = 0; i < claimCache.length; i++) {
-									claimCache[i] = Math.min(claimCache[i], received[i]);
-								}
-							}
 							Database.updateCachedClaimMinHash(claimCache, obj.name);
 
 						} catch(Exception e) {
@@ -84,8 +59,8 @@ public class Patent {
 		System.out.println(obj.name);
 		Patent.lastPubDate=obj.date;
 
-		RecursiveAction addAbstract = new RecursiveAction() {
-			public void compute() {
+		Thread thr = new Thread() {
+			public void run() {
 				try {
 					Database.updateAbstractMinHash(NLP.createMinHash(obj.oAbstract, SimilarityType.ABSTRACT, Main.LEN_SHINGLES), obj.name);
 				} catch (Exception e) {
@@ -94,15 +69,18 @@ public class Patent {
 			}
 		};
 
-		addAbstract.fork();
+		thr.start();
 
 		try {
 			Database.updateDescriptionMinHash(NLP.createMinHash(obj.description,SimilarityType.DESCRIPTION, Main.LEN_SHINGLES), obj.name);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		try {
+			thr.join();
+		} catch (Exception e) {
 
-		addAbstract.join();
+		}
 
 	}
 }
